5758007bf03081c6b0c46dc93438b2d912e8c463
dukecg@gmail.com <dukecg@gmail.com>
2018-04-11 00:55:55 +1000

    gender fixes

--- Source/RPGBase/Public/Gender.h
+++ Source/RPGBase/Public/Gender.h
@@ -2,9 +2,13 @@
 
 #include "CoreMinimal.h"
 
+#include "Gender.generated.h"
+
 UENUM(BlueprintType)
 enum class EGender : uint8
 {
-	G_Male = 0		UMETA(DisplayName = "Male"),
-	G_Female = 1	UMETA(DisplayName = "Female")
+	G_None = 0					UMETA(DisplayName = "None"),
+	G_Male = 1					UMETA(DisplayName = "Male"),
+	G_Female = 2				UMETA(DisplayName = "Female"),
+	G_Any = G_Male | G_Female	UMETA(DisplayName = "Any")
 };
